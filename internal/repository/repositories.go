54a0ede916c631c264fc9b4479dc35de962ac256
Alexander Ponomarev <aaponomarev04@gmail.com>
2023-12-08 23:09:24 +0300

    Rework directories

--- internal/repository/repositories.go
+++ internal/repository/repositories.go
@@ -1,18 +1,18 @@
 package repository
 
 import (
-	provider "github.com/Jira-Analyzer/backend-services/internal/db/postgres"
-	db "github.com/Jira-Analyzer/backend-services/internal/repository/db"
+	provider "github.com/Jira-Analyzer/backend-services/internal/db"
+	db "github.com/Jira-Analyzer/backend-services/internal/repository/psql"
 )
 
 type Repositories struct {
-	*db.IssueRepository
-	*db.ProjectRepository
+	IssueRepositoryInterface
+	ProjectRepositoryInrerface
 }
 
 func NewRepositories(provider *provider.Provider) *Repositories {
 	return &Repositories{
-		IssueRepository:   db.NewIssueRepository(provider),
-		ProjectRepository: db.NewProjectRepository(provider),
+		IssueRepositoryInterface:   db.NewIssueRepository(provider),
+		ProjectRepositoryInrerface: db.NewProjectRepository(provider),
 	}
 }
