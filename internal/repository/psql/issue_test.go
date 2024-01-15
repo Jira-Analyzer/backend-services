bba0d58935b47becf8c6cb2acc02770a88e29a47
Alexander Ponomarev <aaponomarev04@gmail.com>
2024-01-15 21:01:02 +0300

    Use cleanup for testing

--- internal/repository/psql/issue_test.go
+++ internal/repository/psql/issue_test.go
@@ -13,8 +13,8 @@ import (
 	sqlxmock "github.com/zhashkevych/go-sqlxmock"
 )
 
-var issueRows = sqlxmock.NewRows([]string{"id", "project_id", "author_id", "reporter_id", "key", "summary", "type", "priority", "status", "created_time", "closed_time", "updated_time", "time_spent"}).
-	AddRow(12330737, 10730, 1, 1, "AGILA-44", "Option to use JSP Includ…g forms for Task nodes.", "Bug", "Major", "Open", time.Time{}, time.Time{}, time.Time{}, 0)
+var issueRows = sqlxmock.NewRows([]string{"id", "project_id", "author_id", "reporter_id", "key", "summary", "type", "priority", "status", "created_time", "closed_time", "updated_time"}).
+	AddRow(12330737, 10730, 1, 1, "AGILA-44", "Option to use JSP Includ…g forms for Task nodes.", "Bug", "Major", "Open", time.Time{}, time.Time{}, time.Time{})
 
 func TestIssueRepository_GetIssuesByProject(t *testing.T) {
 	db, mock, err := sqlxmock.Newx()
