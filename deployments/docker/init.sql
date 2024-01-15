aa7d8329acb546766ccd8688258c1b9553eca886
Alexander Ponomarev <aaponomarev04@gmail.com>
2024-01-15 20:56:41 +0300

    Remove time_spent column for issue

--- deployments/docker/init.sql
+++ deployments/docker/init.sql
@@ -27,6 +27,5 @@ CREATE TABLE IF NOT EXISTS "Issue" (
   status TEXT,
   created_time TIMESTAMP WITHOUT TIME ZONE,
   closed_time TIMESTAMP WITHOUT TIME ZONE,
-  updated_time TIMESTAMP WITHOUT TIME ZONE,
-  time_spent INT DEFAULT 0
+  updated_time TIMESTAMP WITHOUT TIME ZONE
 );
