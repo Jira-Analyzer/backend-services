2c5966690730b2ebdd612378a4eb7af3f6fbcfce
Alexander Ponomarev <aaponomarev04@gmail.com>
2024-01-15 21:00:13 +0300

    Use custom error for tracing

--- internal/db/provider.go
+++ internal/db/provider.go
@@ -3,6 +3,7 @@ package provider
 import (
 	"fmt"
 
+	errorlib "github.com/Jira-Analyzer/backend-services/internal/error"
 	_ "github.com/jackc/pgx/v5/stdlib"
 	"github.com/jmoiron/sqlx"
 )
@@ -11,18 +12,11 @@ type Provider struct {
 	*sqlx.DB
 }
 
-func (provider *Provider) Close() error {
-	if provider != nil {
-		return provider.DB.Close()
-	}
-	return nil
-}
-
 func NewPsqlProvider(config *Config) (*Provider, error) {
 	connectionFmt := "postgresql://@%s/%s?user=%s&password=%s&sslmode=disable"
 	db, err := sqlx.Open("pgx", fmt.Sprintf(connectionFmt, config.Host, config.Name, config.User, config.Password))
 	if err != nil {
-		return nil, fmt.Errorf("Failed to add database to pool. Error: %w", err)
+		return nil, fmt.Errorf("failed to add database to pool. Error: %w", errorlib.InternalError)
 	}
 
 	return &Provider{
