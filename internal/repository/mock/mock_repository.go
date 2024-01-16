812bcddf103568b7b51b996cf88db20ab57a7630
Alexander Ponomarev <aaponomarev04@gmail.com>
2024-01-16 18:20:48 +0300

    Update mock

--- internal/repository/mock/mock_repository.go
+++ internal/repository/mock/mock_repository.go
@@ -92,3 +92,18 @@ func (mr *MockIProjectRepositoryMockRecorder) GetProjects(arg0 any) *gomock.Call
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "GetProjects", reflect.TypeOf((*MockIProjectRepository)(nil).GetProjects), arg0)
 }
+
+// GetProjectsByRange mocks base method.
+func (m *MockIProjectRepository) GetProjectsByRange(arg0 context.Context, arg1, arg2 int) ([]domain.Project, error) {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "GetProjectsByRange", arg0, arg1, arg2)
+	ret0, _ := ret[0].([]domain.Project)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
+}
+
+// GetProjectsByRange indicates an expected call of GetProjectsByRange.
+func (mr *MockIProjectRepositoryMockRecorder) GetProjectsByRange(arg0, arg1, arg2 any) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "GetProjectsByRange", reflect.TypeOf((*MockIProjectRepository)(nil).GetProjectsByRange), arg0, arg1, arg2)
+}
