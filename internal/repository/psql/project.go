50673862fde543b20096f05273100b3697e64244
Alexander Ponomarev <aaponomarev04@gmail.com>
2024-01-16 18:21:39 +0300

    Add new method to complete interface

--- internal/repository/psql/project.go
+++ internal/repository/psql/project.go
@@ -22,7 +22,17 @@ func NewProjectRepository(provider *provider.Provider) *ProjectRepository {
 func (repository *ProjectRepository) GetProjects(ctx context.Context) ([]domain.Project, error) {
 	projects := make([]domain.Project, 0)
 	if err := repository.db.SelectContext(ctx, &projects, `SELECT * FROM "Project"`); err != nil {
-		return nil, fmt.Errorf("Failed to fetch projects due to: %w", errorlib.InternalError)
+		return nil, fmt.Errorf("failed to fetch projects due to: %w", errorlib.InternalError)
 	}
+
+	return projects, nil
+}
+
+func (repository *ProjectRepository) GetProjectsByRange(ctx context.Context, offset int, count int) ([]domain.Project, error) {
+	projects := make([]domain.Project, 0)
+	if err := repository.db.SelectContext(ctx, &projects, `SELECT * FROM "Project" ORDER BY id OFFSET $1 ROWS FETCH FIRST $2 ROW ONLY`, offset, count); err != nil {
+		return nil, fmt.Errorf("failed to fetch projects due to: %w", err)
+	}
+
 	return projects, nil
 }
