6b4b29bbee3e1e1fc0c1df7e0e8ecc49538dc53f
Alexander Ponomarev <aaponomarev04@gmail.com>
2024-01-19 23:51:38 +0300

    Add config validation

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,22 +3,24 @@ package config
 import (
 	"fmt"
 	"os"
+	"strings"
 	"time"
 
+	errorlib "github.com/Jira-Analyzer/backend-services/internal/error"
 	"gopkg.in/yaml.v3"
 )
 
 type DbConfig struct {
-	Host     string `yaml:"host"`
-	Name     string `yaml:"name"`
-	User     string `yaml:"user"`
-	Password string `yaml:"password"`
+	Host     *string `yaml:"host"`
+	Name     *string `yaml:"name"`
+	User     *string `yaml:"user"`
+	Password *string `yaml:"password"`
 }
 
 type ServerConfig struct {
-	Host             string        `yaml:"host"`
-	ResourceTimeout  time.Duration `yaml:"resource-timeout"`
-	AnalyticsTimeout time.Duration `yaml:"analytics-timeout"`
+	Host             *string        `yaml:"host"`
+	ResourceTimeout  *time.Duration `yaml:"resource-timeout"`
+	AnalyticsTimeout *time.Duration `yaml:"analytics-timeout"`
 }
 
 type Config struct {
@@ -38,4 +40,26 @@ func ReadConfigFromYAML(path string) (*Config, error) {
 	}
 
 	return conf, nil
+}
+
+func ValidateConfig(config *Config) error {
+	var err error = nil
+	fields := make([]string, 0)
+	if config.DbConfig.Host == nil {
+		fields = append(fields, "db.Host")
+	}
+	if config.DbConfig.Name == nil {
+		fields = append(fields, "db.Name")
+	}
+	if config.DbConfig.User == nil {
+		fields = append(fields, "db.User")
+	}
+	if config.DbConfig.Password == nil {
+		fields = append(fields, "db.Password")
+	}
+
+	if len(fields) != 0 {
+		err = fmt.Errorf("Error: %w. Missing fields %s", errorlib.ConfigValueMissError, strings.Join(fields, ", "))
+	}
+	return err
 }
--- internal/error/error.go
+++ internal/error/error.go
@@ -3,3 +3,4 @@ package errorlib
 import "errors"
 
 var InternalError = errors.New("some internal error happened")
+var ConfigValueMissError = errors.New("config value missed")
