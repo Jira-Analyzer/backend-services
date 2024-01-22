958f7c991622de2aeaa40fed16c9354b7606406e
Alexander Ponomarev <aaponomarev04@gmail.com>
2024-01-23 02:18:20 +0300

    Rename erors & add json support

--- go.mod
+++ go.mod
@@ -10,10 +10,16 @@ require (
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/gabriel-vasile/mimetype v1.4.2 // indirect
+	github.com/go-playground/locales v0.14.1 // indirect
+	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/go-playground/validator/v10 v10.17.0 // indirect
 	github.com/kr/text v0.1.0 // indirect
+	github.com/leodido/go-urn v1.2.4 // indirect
 	github.com/lib/pq v1.10.9 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rogpeppe/go-internal v1.11.0 // indirect
+	golang.org/x/net v0.10.0 // indirect
 	golang.org/x/sys v0.8.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- internal/error/error.go
+++ internal/error/error.go
@@ -1,6 +1,34 @@
 package errorlib
 
-import "errors"
+import (
+	"errors"
+	"net/http"
+)
 
-var InternalError = errors.New("some internal error happened")
-var ConfigValueMissError = errors.New("config value missed")
+var ErrHttpInternal = errors.New("some internal error happened")
+var ErrHttpBadRequest = errors.New("probably bad request")
+var ErrHttpInvalidRequestData = errors.New("some data is invalid")
+
+type JSONError struct {
+	Code    int    `json:"code"`
+	Message string `json:"msg"`
+}
+
+func GetJSONError(msg string, err error) *JSONError {
+	var jsonErr = JSONError{
+		Message: msg,
+	}
+
+	switch {
+	case errors.Is(err, ErrHttpInternal):
+		jsonErr.Code = http.StatusInternalServerError
+	case errors.Is(err, ErrHttpBadRequest):
+		jsonErr.Code = http.StatusBadRequest
+	case errors.Is(err, ErrHttpInvalidRequestData):
+		jsonErr.Code = http.StatusUnprocessableEntity
+	default:
+		jsonErr.Code = http.StatusInternalServerError
+	}
+
+	return &jsonErr
+}
