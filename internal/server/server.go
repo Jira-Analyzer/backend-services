d0335aae66a14f43b75be2e7f7a4c76ad040dd89
Alexander Ponomarev <aaponomarev04@gmail.com>
2024-01-23 02:17:36 +0300

    Use values from config for server init

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,7 +1,6 @@
 package server
 
 import (
-	"context"
 	"net/http"
 	"time"
 
@@ -15,29 +14,17 @@ type Server struct {
 	shutdownTimeout time.Duration
 }
 
-func NewServer(config *config.ServerConfig, router *mux.Router) *Server {
-	var (
-		readTimeout     = 5 * time.Second
-		writeTimeout    = 5 * time.Second
-		addr            = ":8000"
-		shutdownTimeout = 3 * time.Second
-	)
-
-	if config.Host != nil {
-		addr = *config.Host
-	}
-
+func NewServer(config *config.ServerConfig, router *mux.Router, notify chan error) *Server {
 	httpServer := &http.Server{
-		Addr:         addr,
+		Addr:         *config.Host,
 		Handler:      router,
-		ReadTimeout:  readTimeout,
-		WriteTimeout: writeTimeout,
+		ReadTimeout:  *config.ReadTimeout,
+		WriteTimeout: *config.WriteTimeout,
 	}
 
 	serv := &Server{
-		server:          httpServer,
-		notify:          make(chan error, 1),
-		shutdownTimeout: shutdownTimeout,
+		server: httpServer,
+		notify: notify,
 	}
 
 	return serv
@@ -46,17 +33,9 @@ func NewServer(config *config.ServerConfig, router *mux.Router) *Server {
 func (s *Server) Start() {
 	go func() {
 		s.notify <- s.server.ListenAndServe()
-		close(s.notify)
 	}()
 }
 
-func (s *Server) GetNotify() <-chan error {
-	return s.notify
-}
-
-func (s *Server) Shutdown() error {
-	ctx, cancel := context.WithTimeout(context.Background(), s.shutdownTimeout)
-	defer cancel()
-
-	return s.server.Shutdown(ctx)
+func (s *Server) Stop() error {
+	return s.server.Close()
 }
