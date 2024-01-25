93b208696cc60b0e79310a3c85745bdbcb6d4652
unknown <flusova.sa@edu.spbstu.ru>
2024-01-25 14:44:57 +0700

    reorganising client.go - attempt 2 (#20)

--- internal/client/jira/client.go
+++ internal/client/jira/client.go
@@ -2,18 +2,24 @@ package jira
 
 import (
 	"encoding/json"
-	"fmt"
+	"errors"
+	"github.com/sirupsen/logrus"
+	"io"
+	"math"
 	"net/http"
+	"strconv"
+	"strings"
+	"sync"
 	"time"
 )
 
 type Client struct {
 	baseURL          string
 	client           *http.Client
-	issuesPerRequest int64
-	threadCount      int64
-	maxTimeSleep     time.Duration
-	minTimeSleep     time.Duration
+	issuesPerRequest int
+	threadCount      int
+	maxTimeSleep     int
+	minTimeSleep     int
 }
 
 func NewClient(config Config) *Client {
@@ -27,8 +33,8 @@ func NewClient(config Config) *Client {
 	}
 }
 
-func (c *Client) FetchProjects() (*FetchProjectResponseDTO, error) {
-	url := fmt.Sprintf("%s/rest/api/2/project", c.baseURL)
+func (c *Client) FetchProjects(limit int, page int, search string, projectKey string) (*ProjectsResponse, error) {
+	/*url := fmt.Sprintf("%s/rest/api/2/project", c.baseURL)
 
 	resp, err := c.client.Get(url)
 	if err != nil {
@@ -41,11 +47,59 @@ func (c *Client) FetchProjects() (*FetchProjectResponseDTO, error) {
 		return nil, err
 	}
 
-	return &dto, nil
+	return &dto, nil*/
+
+	response, err := http.Get(c.baseURL + "/rest/api/2/project")
+	if err != nil {
+		logrus.Error(logrus.Error, "Unable to get projects list ")
+		return &ProjectsResponse{}, err
+	}
+
+	body, err := io.ReadAll(response.Body)
+
+	if err != nil {
+		return &ProjectsResponse{}, err
+	}
+
+	var jiraProjects []JiraProject
+	err = json.Unmarshal(body, &jiraProjects)
+
+	if err != nil {
+		return &ProjectsResponse{}, err
+	}
+
+	var projects []Project
+
+	projectsCount := 0
+
+	for _, elem := range jiraProjects {
+		if strings.Contains(strings.ToLower(projectKey), strings.ToLower(search)) {
+			projectsCount++
+			projects = append(projects, Project{
+				Name: elem.Name,
+				Link: elem.Link,
+			})
+		}
+	}
+
+	startIndex := limit * (page - 1)
+	endIndex := startIndex + limit
+	if endIndex >= len(projects) {
+		endIndex = len(projects)
+	}
+
+	return &ProjectsResponse{
+		Projects: projects[startIndex:endIndex],
+		PageInfo: PageInfo{
+			PageCount:     int(math.Ceil(float64(projectsCount) / float64(limit))),
+			CurrentPage:   page,
+			ProjectsCount: projectsCount,
+		},
+	}, nil
 }
 
-func (c *Client) FetchIssues(projectKey string) (*FetchIssueResponseDTO, error) {
-	url := fmt.Sprintf("%s/rest/api/2/search?jql=project=%s", c.baseURL, projectKey)
+func (c *Client) FetchIssues(projectKey string, timeToWaitMs int) (map[Issue]struct{}, error) {
+	/*url := fmt.Sprintf("%s/rest/api/2/search?jql=project=%s", c.baseURL, projectKey)
 
 	resp, err := c.client.Get(url)
 	if err != nil {
@@ -58,5 +112,94 @@ func (c *Client) FetchIssues(projectKey string) (*FetchIssueResponseDTO, error) 
 		return nil, err
 	}
 
-	return &dto, nil
+	return &dto, nil*/
+	httpClient := &http.Client{}
+	response, err := httpClient.Get(c.baseURL +
+		"/rest/api/2/search?jql=project=" + projectKey + "&expand=changelog&startAt=0&maxResults=1")
+
+	if err != nil || response.StatusCode != http.StatusOK {
+		logrus.Error(logrus.Error, "Unable to get issues for project "+projectKey)
+		return map[Issue]struct{}{}, nil
+	}
+
+	body, _ := io.ReadAll(response.Body)
+	var issueResponse IssuesList
+	if err = json.Unmarshal(body, &issueResponse); err != nil {
+		logrus.Error(logrus.Error, "Error while unmarshalling issue response")
+	}
+
+	totalIssuesCount := issueResponse.IssuesCount
+
+	if totalIssuesCount == 0 {
+		return map[Issue]struct{}{}, nil
+	}
+
+	issues := map[Issue]struct{}{}
+	issues[issueResponse.Issues[0]] = struct{}{}
+
+	waitGroup := sync.WaitGroup{}
+	mutex := sync.Mutex{}
+	wasError := false
+
+	threadCount := c.threadCount
+	issuesPerRequest := c.issuesPerRequest
+
+	stop := make(chan struct{})
+
+	for i := 0; i < threadCount; i++ {
+		waitGroup.Add(1)
+		go func(threadNumber int) {
+			defer waitGroup.Done()
+			select {
+			case <-stop:
+				logrus.Error(logrus.Error, "Error while reading issues in thread... Stopping all other threads...")
+				return
+			default:
+				threadStartIndex := (totalIssuesCount/threadCount)*threadNumber + 1
+				requestCount := int(math.Ceil(float64(totalIssuesCount) / float64(threadCount*issuesPerRequest)))
+				for j := 0; j < requestCount; j++ {
+					startAt := threadStartIndex + j*issuesPerRequest
+					if startAt < totalIssuesCount {
+						requestString := c.baseURL + "/rest/api/2/search?jql=project=" +
+							projectKey + "&expand=changelog&startAt=" + strconv.Itoa(startAt) +
+							"&maxResults=" + strconv.Itoa(issuesPerRequest)
+
+						response, requestErr := httpClient.Get(requestString)
+						body, responseReadErr := io.ReadAll(response.Body)
+
+						if requestErr != nil || responseReadErr != nil {
+							wasError = true
+							close(stop)
+							return
+						}
+
+						var issueResponse IssuesList
+						_ = json.Unmarshal(body, &issueResponse)
+
+						mutex.Lock()
+						for _, elem := range issueResponse.Issues {
+							issues[elem] = struct{}{}
+						}
+						mutex.Unlock()
+					}
+				}
+			}
+		}(i)
+	}
+	waitGroup.Wait()
+
+	if wasError {
+		time.Sleep(time.Duration(timeToWaitMs) * time.Millisecond)
+		newTimeToSleep := int(math.Ceil(float64(timeToWaitMs) * math.Phi))
+		logrus.Error(logrus.Info, "Error while downloading issues for project \""+
+			projectKey+"\", waiting now"+strconv.Itoa(timeToWaitMs)+"ms")
+
+		if newTimeToSleep > c.maxTimeSleep {
+			return map[Issue]struct{}{}, errors.New("A lot of time to sleep")
+		}
+
+		return c.FetchIssues(projectKey, newTimeToSleep)
+	}
+
+	return issues, nil
 }
--- internal/client/jira/config.go
+++ internal/client/jira/config.go
@@ -1,13 +1,9 @@
 package jira
 
-import (
-	"time"
-)
-
 type Config struct {
-	JiraUrl          string        `yaml:"jiraUrl"`
-	IssuesPerRequest int64         `yaml:"issuesPerRequest"`
-	ThreadCount      int64         `yaml:"threadCount"`
-	MaxTimeSleep     time.Duration `yaml:"maxTimeSleep"`
-	MinTimeSleep     time.Duration `yaml:"minTimeSleep"`
+	JiraUrl          string `yaml:"jiraUrl"`
+	IssuesPerRequest int    `yaml:"issuesPerRequest"`
+	ThreadCount      int    `yaml:"threadCount"`
+	MaxTimeSleep     int    `yaml:"maxTimeSleep"`
+	MinTimeSleep     int    `yaml:"minTimeSleep"`
 }
